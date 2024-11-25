dd4756ee1a004f34388e82704b1aed4748f5cb60
Endercat9999 <2005martarosa@gmail.com>
2024-11-25 11:47:23 +0100

    tilmap camara

---
+++ Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    public static bool isGrounded; 
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.layer == 3)
+        {
+            isGrounded = true;
+            PlayerControler.characterAnimator.SetBool("IsJumping", false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.layer == 6)
+        {
+            isGrounded = false;
+            PlayerControler.characterAnimator.SetBool("IsJumping", true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/strella.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class strella : MonoBehaviour
+{
+   
+private bool interectable;
+           void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && interectable)
+        {
+SceneManager.LoadScene("Victoria");
+         }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            interectable = true;
+        }
+    }
+
+     void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+             
+            interectable = false;
+        }
+    }
+
+}
